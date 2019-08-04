6198d6c4a1a178149940da8e3a086a1c8cd626c8
Deepak <Deepak@pacdevelopers.com>
2019-08-04 18:28:21 +0530

    model

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -17,4 +17,34 @@ class LoginModel extends CI_Model
     	$this->db->insert('image',$data);
 
     } 
+
+      function deletePivotUserDepartment($table,$id){
+        $this->db->where('user_id',$id);
+        $this->db->delete($table);
+        return $this->db->affected_rows();
+    }
+    function deleteRow($table,$id){
+        $this->db->where('ID',$id);
+        $this->db->delete($table);
+        return $this->db->affected_rows();
+    }
+    function deleteTask($order_no){
+        $this->db->where('order_no',$order_no);
+        $this->db->delete('chamton_tasks');
+        return $this->db->affected_rows();
+    }
+    function editRow($table,$condition,$data){
+        $result=$this->db->update($table,$data,$condition);
+        return $result;
+        //return $data;
+    }
+
+    function productList(){
+        $this->db->select('product_list.*,product_types.CODE');
+        $this->db->from('product_list');
+        $this->db->join('product_types','product_list.PRODUCT_TYPE = product_types.ID','left');
+        $this->db->order_by('product_list.ID','desc');
+        $rows=$this->db->get()->result_array();
+        return $rows;
+    }
 }
